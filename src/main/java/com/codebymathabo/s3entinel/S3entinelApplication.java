da16c9652ad4bc7344f7eaa77b19d84e996d3567
Mathabo Motaung <moatung20mathabo>
2025-12-23 01:27:30 +0200

    Initial commit

---
+++ src/main/java/com/codebymathabo/s3entinel/S3entinelApplication.java
@@ -0,0 +1,13 @@
+package com.codebymathabo.s3entinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3entinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3entinelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codebymathabo/s3entinel/S3entinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebymathabo.s3entinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3entinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
